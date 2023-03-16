820517c10acd68b97a983001260cc453d768ac48
ahmedehab0 <ahmed.eh@gmail.com>
2023-03-16 16:07:11 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-/**
-*main-entery
-*
-*Return:Always 0
-int main (void)
-{
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+ 1 #include <stdio.h>
+  2 /**
+  3 *main-entery
+  *
+  5 *Return:Always 0
+  */
+  6 int main (void)
+  7 {
+  8 printf ("with proper grammar, but the outcome is a piece of art,\n");
+  9 return (0);
+ 10 }
