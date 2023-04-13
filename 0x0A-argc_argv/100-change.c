d66e25cbe5aaaf77fd9593053b1b132b14fea8a8
ahmedehab0 <ahmed.eh@gmail.com>
2023-04-13 15:46:22 -0700

    0

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
+ */
+
+int main(int argc, char *argv[])
+{
+int i = atoi(argv[1]), count = 0,
+changes[] = {1, 2, 5, 10, 25}, j = 4;
+if (argc == 2)
+while (i != 0)
+{
+if (i - changes[j] > 0)
+{
+i -= changes [j];
+count++;
+}
+else
+{
+	j--;
+}
+printf("%d\n", count);
+}
+else
+printf("error");
+
+return (0);
+}
