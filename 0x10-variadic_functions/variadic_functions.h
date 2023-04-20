fbc96409fd9fb6cd5dc3c2f561648624ea0e96b2
ahmedehab0 <ahmed.eh@gmail.com>
2023-04-20 13:12:50 -0700

    fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,9 @@
 #define _variadic_functions_h_
 
 int sum_them_all(const unsigned int n, ...);
-
+int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void print_all(const char * const format, ...);
 
 #endif /* _variadic_functions_h_ */
