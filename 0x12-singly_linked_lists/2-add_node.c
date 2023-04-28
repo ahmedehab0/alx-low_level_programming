4aacbd5ec5b3c84bdf2fcfbf2d970d2d99de1b09
ahmedehab0 <ahmed.eh@gmail.com>
2023-04-27 19:00:28 -0700

    fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,17 +17,17 @@ list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node)
 	{
+		if (strdup(str) == NULL)
+		{
+			return (NULL);
+			free(new_node);
+		}
 		while (str[len])
 			len++;
 
 		new_node->str = strdup(str);
 		new_node->next = *head;
 		new_node->len = len;
-
-		if (strdup(str) == NULL){
-		return (NULL);
-		free(new_node);
-		}
 
 		*head = new_node;
 		return (new_node);
