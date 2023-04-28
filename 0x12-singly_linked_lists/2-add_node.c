06bb2843329777488e80379a707845d09cc99af4
ahmedehab0 <ahmed.eh@gmail.com>
2023-04-27 18:28:37 -0700

    insert node at the begenning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+list_t *add_node(list_t **head, const char *str);
+{
+int len = 0;
+list_t new_node = malloc(sizeof(list_t));
+
+	if (new_node)
+	{
+		new_node->str = strdub(str);
+		new_node->next = *head;
+		new_node->len = len;
+
+		if (strdub(str) == NULL){
+		return (NULL);
+		free(new_node);
+		}
+
+		while(str[len])
+		len++;
+
+		*head = new_node;
+		return (&new_node);
+	}
+
+	return (NULL);
+
+}
