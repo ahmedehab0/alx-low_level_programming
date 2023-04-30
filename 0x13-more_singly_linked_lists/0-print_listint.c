9ca722b45855f30e3f7c4806fb29d2a33c218d3c
ahmedehab0 <ahmed.eh01@gmail.com>
2023-04-30 14:16:05 -0400

    fixed betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_listint - Prints all elements of a list
+ * @h: Pointer to a list
+ *
+ * Return: Number of nodes
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
