36bcafe13c41ccfaa1a85be409c9fd09bfae86a7
ahmedehab0 <ahmed.eh01@gmail.com>
2023-05-04 18:53:14 -0400

    fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if ((b[i] != '0' && b[i] != '1') || b[i] == NULL)
 			return (0);
 
 		result <<= 1;
