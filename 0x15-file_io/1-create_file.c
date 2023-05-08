fd287fc38995821674221d2247829b39ca4651de
ahmedehab0 <ahmed.eh01@gmail.com>
2023-05-08 19:57:31 -0400

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,10 @@ int create_file(const char *filename, char *text_content)
 			text_content = "";
 
 		write(fd, text_content, strlen(text_content));
+		close(fd);
 		return (1);
 	}
+
+	close(fd);
 	return (-1);
 }
