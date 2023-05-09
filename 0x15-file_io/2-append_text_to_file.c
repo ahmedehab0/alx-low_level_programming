aaca40b9f90979edebfd215088cb2e516f060cdf
ahmedehab0 <ahmed.eh01@gmail.com>
2023-05-08 20:26:55 -0400

    fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		close(fd);
