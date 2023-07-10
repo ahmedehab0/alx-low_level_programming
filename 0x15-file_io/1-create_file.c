4ec11a5a7ab5d4366a8498b17572c91ef1cc90c6
ahmedehab0 <ahmed.eh01@gmail.com>
2023-07-10 13:22:17 -0400

    new file incoming

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	else
+	{
+		bytes_written = write(fd, text_content, sizeof(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,6 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int creat_file(const char *filename, char *text_content);
 
 #endif
