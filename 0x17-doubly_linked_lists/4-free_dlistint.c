c3b421554e8cab964c74d7e148c687514b6ba5a3
ahmedehab0 <ahmed.eh01@gmail.com>
2023-08-17 19:57:34 -0400

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,11 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *current = head;
-	
-    	while (head != NULL)
-    	{
-        	current = head;
-        	head = head->next;
-        	free(current);
-    	}
+	dlistint_t *current;
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
