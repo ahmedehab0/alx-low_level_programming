10f565ce43c5c4d603b5a4e1c52d7bd01dfc46c1
ahmedehab0 <ahmed.eh01@gmail.com>
2023-09-07 22:12:16 -0400

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,21 +11,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
 	hash_node_t *node;
+	unsigned long int index;
 
-	if (key == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
+
 	node = ht->array[index];
-	while (node)
-	{
-		if (!strcmp(node->key, key))
-			return (node->value);
+	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
-	}
-	return (NULL);
+
+	return ((node == NULL) ? NULL : node->value);
 }
